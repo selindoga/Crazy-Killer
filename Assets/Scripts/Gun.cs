5e2c473d507e80f81b426532250ffc73cd288a7d
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-07-13 15:18:07 +0300

    making shooting with raycast

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera cam;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
