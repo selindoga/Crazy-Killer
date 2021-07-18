aee1e3fdefe8fa347f314528686ab617849f6e86
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-07-18 12:20:47 +0300

    Update MouseLook.cs

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 75f;
+    private float mouseSensitivity = 65f;
     public Transform playerBody;
     float xRotation;
 
